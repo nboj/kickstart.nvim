08254233c094fb742f4824bef5838e235476527d
Christian Auman <christian.auman@outlook.com>
2024-05-16 11:20:08 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -208,6 +208,19 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+      }
+      vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
