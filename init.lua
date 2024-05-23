5716dafab3651bda3ff58b299acbc4bc4d3884ae
Christian Auman <christian.auman@outlook.com>
2024-05-23 12:36:23 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -209,11 +209,72 @@ require('lazy').setup({
   },
 
   {
+    'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('nvim-web-devicons').setup {
+        -- your personnal icons can go here (to override)
+        -- you can specify color or cterm_color instead of specifying both of them
+        -- DevIcon will be appended to `name`
+        override = {
+          zsh = {
+            icon = '',
+            color = '#428850',
+            cterm_color = '65',
+            name = 'Zsh',
+          },
+        },
+        -- globally enable different highlight colors per icon (default to true)
+        -- if set to false all icons will have the default icon's color
+        color_icons = true,
+        -- globally enable default icons (default to false)
+        -- will get overriden by `get_icons` option
+        default = true,
+        -- globally enable "strict" selection of icons - icon will be looked up in
+        -- different tables, first by filename, and if not found by extension; this
+        -- prevents cases when file doesn't have any extension but still gets some icon
+        -- because its name happened to match some extension (default to false)
+        strict = true,
+        -- same as `override` but specifically for overrides by filename
+        -- takes effect when `strict` is true
+        override_by_filename = {
+          ['.gitignore'] = {
+            icon = '',
+            color = '#f1502f',
+            name = 'Gitignore',
+          },
+        },
+        -- same as `override` but specifically for overrides by extension
+        -- takes effect when `strict` is true
+        override_by_extension = {
+          ['log'] = {
+            icon = '',
+            color = '#81e043',
+            name = 'Log',
+          },
+        },
+        -- same as `override` but specifically for operating system
+        -- takes effect when `strict` is true
+        override_by_operating_system = {
+          ['apple'] = {
+            icon = '',
+            color = '#A2AAAD',
+            cterm_color = '248',
+            name = 'Apple',
+          },
+        },
+      }
+    end,
+  },
+
+  {
     'stevearc/oil.nvim',
     opts = {},
     config = function()
       require('oil').setup {
         columns = { 'icon' },
+        view_options = {
+          show_hidden = true,
+        },
       }
       vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
