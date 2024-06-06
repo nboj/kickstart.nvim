7ea422faadce70ec2cb71e5acea1b6b9d14751bf
Christian Auman <christian.auman@outlook.com>
2024-06-06 18:04:41 -0400

    modular

--- init.lua
+++ init.lua
@@ -4,12 +4,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require 'custom.plugins.config'
-
 vim.g.python3_host_prog = '/usr/bin/python3.10'
 --vim.g.python3_host_prog = '/home/christianauman/GitHub/kent-SURE-research/daam/env/bin/python'
 
-require 'custom.plugins.keymaps'
+require 'config'
+require 'keymaps'
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -42,64 +41,7 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-  require 'custom.plugins.telescope',
-  require 'custom.plugins.harpoon',
-  require 'custom.plugins.jupyter',
-  require 'custom.plugins.webdevicons',
-  require 'custom.plugins.oil',
-  require 'custom.plugins.init',
-  require 'custom.plugins.gitsigns',
-  require 'custom.plugins.whichkey',
-  require 'custom.plugins.lspconfig',
-  require 'custom.plugins.autoformat',
-  require 'custom.plugins.autocompletion',
-  require 'custom.plugins.theme',
-  require 'custom.plugins.treesitter',
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+require('lazy').setup 'plugins'
 
 -- The line beneath this is called `modeline`.
 -- vim: ts=2 sts=2 sw=2 et
