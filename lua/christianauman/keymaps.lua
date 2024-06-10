921ae70116baa8bd24a31e5aeeff2fe1082e9a15
Christian Auman <christian.auman@outlook.com>
2024-06-10 12:07:55 -0400

    Update keymaps.lua

--- lua/christianauman/keymaps.lua
+++ lua/christianauman/keymaps.lua
@@ -3,10 +3,15 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>p', '"*p')
 vim.keymap.set('n', '<leader>b', '<cmd>e .<CR>')
 vim.keymap.set('n', '<A-j>', '<cmd>m +1<CR>')
 vim.keymap.set('n', '<A-k>', '<cmd>m -2<CR>')
+
+-- Custom Keymaps
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>y', '"+y')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -16,15 +21,6 @@ vim.keymap.set('n', '<A-k>', '<cmd>m -2<CR>')
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
 --vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 --vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
